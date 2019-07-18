4820d3f7beac599f172d7bce8a968afccf9b501a
STIG TOP GEAR <vmv9753@gmail.com>
2019-07-18 16:45:19 +0300

    1 task

---
+++ hw11/task11.1/bruh.js
@@ -0,0 +1,25 @@
+var memes
+
+const promise = new Promise(
+	function(resolve, reject) {
+		const request = new XMLHttpRequest
+		request.open("GET","https://github.com/HOUYUSTON/js-hw/tree/master/hw11_1")
+		request.onreadystatechange = function (event) {
+			this.readyState === 4 ?
+				this.status === 200 ?
+					resolve(this.responseText)
+		}
+	}
+).then (
+	text => memes = JSON.parse(text)
+)
+
+memes.forEach(
+	function (meme) {
+		let div = document.body.appendChild(document.createElement("div"))
+		let img = div.appendChild(document.createElement("img"))
+		let p = div.appendChild(document.createElement("p"))
+		img.src = meme.ref
+		p.textContent = meme.title
+	}
+)
